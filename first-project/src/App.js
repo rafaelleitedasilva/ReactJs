f574b01f93df719fb50706b1af3c26851bdf3908
Rafael <69431917+rafaelleitedasilva@users.noreply.github.com>
2022-08-19 16:54:56 -0300

    banner with banner.js function

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import './componentes/Banner/Banner.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        Banner()
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ first-project/src/componentes/Banner/Banner.js
@@ -0,0 +1,5 @@
+function Banner(){
+    return(
+        <img src="../public/img/banner.png"/>
+    )
+}
